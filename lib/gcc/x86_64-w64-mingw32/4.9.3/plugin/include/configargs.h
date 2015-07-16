8d514841cb2fabb53212e44d1ddd2546db3525fc
Dmitry Moskalchuk <dm@crystax.net>
2015-07-17 02:00:34 +0300

    Initial commit

---
+++ lib/gcc/x86_64-w64-mingw32/4.9.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/tmp/build-mingw64-toolchain-crystax/src/gcc-4.9.3/configure --prefix=/tmp/build-mingw64-toolchain-crystax/install-x86_64-linux-gnu/x86_64-w64-mingw32 --disable-shared --with-gmp=/tmp/build-mingw64-toolchain-crystax/install-x86_64-linux-gnu/x86_64-w64-mingw32 --with-mpfr=/tmp/build-mingw64-toolchain-crystax/install-x86_64-linux-gnu/x86_64-w64-mingw32 --with-mpc=/tmp/build-mingw64-toolchain-crystax/install-x86_64-linux-gnu/x86_64-w64-mingw32 --with-isl=/tmp/build-mingw64-toolchain-crystax/install-x86_64-linux-gnu/x86_64-w64-mingw32 --with-cloog=/tmp/build-mingw64-toolchain-crystax/install-x86_64-linux-gnu/x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --enable-targets=all --enable-languages=c,c++ --with-sysroot=/tmp/build-mingw64-toolchain-crystax/install-x86_64-linux-gnu/x86_64-w64-mingw32 --enable-threads=posix";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu_32", "generic" }, { "cpu_64", "generic" }, { "arch", "x86-64" } };
